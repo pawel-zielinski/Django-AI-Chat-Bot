5f5027e9c7a0d32bacfb41e69eed8733ce504cf6
Pawel Zielinski <pawelzet00.pz@gmail.com>
2024-10-08 22:30:53 +0200

    Remove key

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = "django-insecure--1#@o3@16=9m2yptfd5klgn(0mu2n&%)9ubx)_!%#p+@sw%i*!"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
