93a609a4abb20ada66cd1e67760fa3dfc39e3fbd
Piotr-Zielinski-PZ <piotrzet00@outlook.com>
2024-10-30 09:34:48 +0100

    Tweak the pipelines and retrigger job

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv("DOMAIN_NAME")]
+ALLOWED_HOSTS = [os.getenv("DOMAIN_NAME"), "*"]
 
 
 # Application definition
