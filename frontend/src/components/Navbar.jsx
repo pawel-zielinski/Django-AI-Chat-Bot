0e3025599baeec7fe95a8f94f787818084c1623e
Piotr-Zielinski-PZ <piotrzet00@outlook.com>
2024-11-25 08:57:05 +0100

    Style navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,48 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState, useEffect} from 'react';
+import logo from '../assets/images/logo.png';
 
 const RenderNavbar = () => {
    const [isAuth, setIsAuth] = useState(false);
+   const isTokenExpired = (token) => {
+       const payload = JSON.parse(atob(token.split('.')[1]));
+       return payload.exp < Date.now() / 1000;
+   };
    useEffect(() => {
-       if (localStorage.getItem('access_token') !== null) {
-           setIsAuth(true);
-       }
+       if (localStorage.getItem('access_token') !== null && !isTokenExpired(localStorage.getItem('access_token'))) {
+            setIsAuth(true);
+        }
    }, [isAuth]);
    return (
       <>
-        <Navbar bg="dark" variant="dark">
-          <Nav className="me-auto">
-          {isAuth ? <Nav.Link href="/">Home</Nav.Link> : null}
-          </Nav>
-          <Nav>
-          {isAuth ? <Nav.Link href="/logout">Logout</Nav.Link> :
-                    <Nav.Link href="/login">Login</Nav.Link>}
-          </Nav>
-        </Navbar>
-       </>
+          <div className={"navbar-component"}>
+              <Navbar bg="dark" variant="dark">
+                  <ul>
+                        <li>
+                            <Navbar.Brand href="/">
+                                <img
+                                    alt=""
+                                    src={logo}
+                                    height="20px"
+                                />
+                            </Navbar.Brand>
+                        </li>
+                      <li>
+                          <Nav>
+                              {isAuth ? <Nav.Link href="/">Home</Nav.Link> : null}
+                          </Nav>
+                      </li>
+                      <li>
+                          <Nav>
+                              {isAuth ? <Nav.Link href="/logout">Logout</Nav.Link> :
+                                  <Nav.Link href="/login">Login</Nav.Link>}
+                          </Nav>
+                      </li>
+                  </ul>
+              </Navbar>
+          </div>
+      </>
    );
 }
 
